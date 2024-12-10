29beac990682d76e2647188510a3ec841a046fc9
mrgemini <ijikawisdom@gmail.com>
2024-12-10 17:42:49 +0100

    smart contract lottery

---
+++ test/integration/Interactions.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+/*
+unit test
+integration test
+forked test
+staging <--- run tests on a mainnet or testnet 
+fuzzing
+stateful fuzz
+stateless fuzz
+formal verification
+*/
+
+pragma solidity 0.8.19;
